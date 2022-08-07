867eb3500f8fa8e1c215332384e57b0085123eef
Uli Heller <uli.heller@daemons-point.com>
2022-08-07 11:49:56 +0200

    Don't use timedatctl

--- create-32bit-image.sh
+++ create-32bit-image.sh
@@ -68,7 +68,7 @@ sudo chroot "./${OSDIR}" apt-get install -y apt-utils net-tools
 sudo chroot "./${OSDIR}" apt-get install -y ca-certificates libpsl5 openssl publicsuffix
 sudo chroot "./${OSDIR}" apt-get install -y less vim
 sudo chroot "./${OSDIR}" bash -c "DEBIAN_FRONTEND=noninteractive apt-get install -y tzdata"
-sudo chroot "./${OSDIR}" timedatectl set-timezone Europe/Berlin
+echo "Europe/Berlin"|sudo tee -a "./${OSDIR}/etc/timezone"
 
 # netplan.io
 # libnetplan0
