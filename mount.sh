a54f34c4cce0cf107ee797149e2b537dc76d45ed
Uli Heller <uli.heller@daemons-point.com>
2022-12-25 11:00:03 +0100

    Create mount points in case they are missing

--- mount.sh
+++ mount.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
+BN="$(basename "$0")"
 TARGET="$1"
 
-mount --bind /dev "${TARGET}/dev"
+for d in "${TARGET}" "${TARGET}/etc" "${TARGET}/bin"; do
+    test -d "${d}" || { echo >&2 "${BN}: Illegal target folder '${TARGET}' - '${d}'"; exit 1; }
+done
+
+for d in dev proc sys; do
+    test -d "${TARGET}/${d}" || mkdir "${TARGET}/${d}"
+    mount --bind "/${d}" "${TARGET}/${d}"
+done
 mount --bind /dev/pts "${TARGET}/dev/pts"
-mount --bind /proc "${TARGET}/proc"
-#mount --bind /run "${TARGET}/run"
-mount --bind /sys "${TARGET}/sys"
