992f9ec64dfb1e1924a586c5c3530693b2a75802
Uli Heller <uli.heller@daemons-point.com>
2025-01-01 10:35:04 +0100

    umount.sh: Added some checks

--- umount.sh
+++ umount.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
+BN="$(basename "$0")"
 TARGET="$1"
+
+test -z "${TARGET}" && {
+    echo >&2 "${BN}: Kein Zielverzeichnis angegeben"
+    exit 1
+}
+
+test -d "${TARGET}" || {
+    echo >&2 "${BN}: Zielverzeichnis '${TARGET}' existiert nicht"
+    exit 1
+}
 
 #set -x
 for m in dev/pts dev proc sys; do
