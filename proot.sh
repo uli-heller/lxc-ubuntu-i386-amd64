c02b82c7eaa11f826d69a12b735b69dd8c5c2515
Uli Heller <uli.heller@daemons-point.com>
2025-01-25 00:23:39 +0100

    proot.sh: Use bind mount for ppa

--- proot.sh
+++ proot.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-exec proot -0 -w / -b /dev -b /dev/pts -b /proc -b /sys -r "$@"
+D="$(dirname "$0")"
+ROOTFS="$(realpath $1)"
+FOLDER="$(echo "${ROOTFS}"|sed -e 's,/rootfs.*$,,')"
+OS_ARCH="$(echo "${FOLDER}"|grep -o -- '-[^-]*-[^-]*$')"
+OS="$(echo "${OS_ARCH}"|cut -d "-" -f 2)"
+ARCH="$(echo "${OS_ARCH}"|cut -d "-" -f 3)"
+
+exec proot -0 -w / -b /dev -b /dev/pts -b /proc -b /sys -b "$(realpath "${D}/ppas/${ARCH}/${OS}"):/var/cache/${OS}" -r "$@"
